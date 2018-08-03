78d759b18a71f82cacd82c400537b824ffdeb916
Pooya <pouyaashna@gmail.com>
2018-08-03 13:55:06 +0430

    add new api to create candidate from careerPage

--- src/main/java/io/avand/web/rest/CareerPageResource.java
+++ src/main/java/io/avand/web/rest/CareerPageResource.java
@@ -1,8 +1,10 @@
 package io.avand.web.rest;
 
 import io.avand.config.ApplicationProperties;
+import io.avand.service.CandidateService;
 import io.avand.service.CompanyService;
 import io.avand.service.JobService;
+import io.avand.service.dto.CandidateDTO;
 import io.avand.service.dto.CompanyDTO;
 import io.avand.service.dto.JobDTO;
 import io.avand.web.rest.errors.ServerErrorException;
@@ -13,11 +15,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,17 +29,21 @@ public class CareerPageResource {
     private final CompanyService companyService;
     private final ApplicationProperties applicationProperties;
     private final JobService jobService;
+    private final CandidateService candidateService;
 
     public CareerPageResource(CompanyService companyService,
                               ApplicationProperties applicationProperties,
-                              JobService jobService) {
+                              JobService jobService,
+                              CandidateService candidateService) {
         this.companyService = companyService;
         this.applicationProperties = applicationProperties;
         this.jobService = jobService;
+        this.candidateService = candidateService;
     }
 
     @GetMapping("/company/{subDomain}")
-    public ResponseEntity getCompanyInfo(@PathVariable("subDomain") String subDomain) {
+    public ResponseEntity getCompanyInfo(@PathVariable("subDomain") String subDomain,
+                                         HttpServletResponse response) {
         log.debug("REST Request to get info of company : {}", subDomain);
         try {
             CompanyDTO companyDTO = companyService.findBySubDomain(subDomain);
@@ -48,7 +52,7 @@ public class CareerPageResource {
             companyVM.setNameEn(companyDTO.getNameEn());
             companyVM.setDescriptionFa(companyDTO.getDescriptionFa());
             companyVM.setDescriptionEn(companyDTO.getDescriptionEn());
-            companyVM.setFilePath(applicationProperties.getBase().getUrl() + "/api/file/load/"+companyDTO.getFileId());
+            companyVM.setFilePath(applicationProperties.getBase().getUrl() + "/api/file/load/" + companyDTO.getFileId());
             return new ResponseEntity<>(companyVM, HttpStatus.OK);
         } catch (NotFoundException e) {
             throw new ServerErrorException(e.getMessage());
@@ -56,19 +60,32 @@ public class CareerPageResource {
     }
 
     @GetMapping("/job/{subDomain}")
-    public ResponseEntity getJobInfo(@PathVariable("subDomain") String subDomain){
-        log.debug("REST Request to get info of job : {}",subDomain);
+    public ResponseEntity getJobInfo(@PathVariable("subDomain") String subDomain) {
+        log.debug("REST Request to get info of job : {}", subDomain);
         List<JobDTO> jobDTOS = jobService.findAllByCompanySubDomain(subDomain);
         List<CareerPageJobVM> jobVMS = new ArrayList<>();
         for (JobDTO jobDTO : jobDTOS) {
             CareerPageJobVM jobVM = new CareerPageJobVM();
+            jobVM.setId(jobDTO.getId());
             jobVM.setName(jobDTO.getName());
             jobVM.setType(jobDTO.getType());
             jobVM.setLocation(jobDTO.getLocation());
             jobVMS.add(jobVM);
         }
 
-        return new ResponseEntity<>(jobVMS,HttpStatus.OK);
+        return new ResponseEntity<>(jobVMS, HttpStatus.OK);
+    }
+
+    @PostMapping("/candidate/{subDomain}")
+    public ResponseEntity createCandidate(@RequestBody CandidateDTO candidateDTO,
+                                          @PathVariable("subDomain") String subDomain) {
+        log.debug("REST Request to create candidate : {}", subDomain);
+        try {
+            CandidateDTO result = candidateService.save(candidateDTO, subDomain);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (NotFoundException | SecurityException e) {
+            throw new ServerErrorException(e.getMessage());
+        }
     }
 
 }

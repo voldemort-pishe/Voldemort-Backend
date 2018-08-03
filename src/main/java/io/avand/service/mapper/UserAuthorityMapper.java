7dbe4d5d5c0d08679f637bb8f71b8f26cad614f5
Pooya <pouyaashna@gmail.com>
2018-08-03 13:50:27 +0430

    fix userId in authority mapper

--- src/main/java/io/avand/service/mapper/UserAuthorityMapper.java
+++ src/main/java/io/avand/service/mapper/UserAuthorityMapper.java
@@ -3,7 +3,12 @@ package io.avand.service.mapper;
 import io.avand.domain.entity.jpa.UserAuthorityEntity;
 import io.avand.service.dto.UserAuthorityDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring",uses = {UserPermissionMapper.class})
 public interface UserAuthorityMapper extends EntityMapper<UserAuthorityDTO, UserAuthorityEntity> {
+
+    @Override
+    @Mapping(source = "user.id",target = "userId")
+    UserAuthorityDTO toDto(UserAuthorityEntity entity);
 }

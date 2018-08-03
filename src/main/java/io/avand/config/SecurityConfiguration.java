a5ec1c5ace138bff3d837ccad19d9d424f1865b0
ehdi <i@ehdi.in>
2018-08-03 13:00:40 +0430

    Permit all payment callback endpoint

--- src/main/java/io/avand/config/SecurityConfiguration.java
+++ src/main/java/io/avand/config/SecurityConfiguration.java
@@ -106,6 +106,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/career-page/**").permitAll()
             .antMatchers("/api/file/load/**").permitAll()
+            .antMatchers("/api/payment/callback/**").permitAll()
             .antMatchers("/api/**").authenticated()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)

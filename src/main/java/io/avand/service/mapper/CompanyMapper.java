cb50aa6ecd43cb21f7373944abb72c78f4fbf6b5
Pooya <pouyaashna@gmail.com>
2018-08-19 21:45:48 +0430

    add companyIncludeVm and vmMapper

--- src/main/java/io/avand/service/mapper/CompanyMapper.java
+++ src/main/java/io/avand/service/mapper/CompanyMapper.java
@@ -2,13 +2,14 @@ package io.avand.service.mapper;
 
 import io.avand.domain.entity.jpa.CompanyEntity;
 import io.avand.service.dto.CompanyDTO;
+import io.avand.web.rest.vm.response.CompanyIncludeVM;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring",uses = {JobMapper.class,EvaluationCriteriaMapper.class})
-public interface CompanyMapper extends EntityMapper<CompanyDTO, CompanyEntity> {
+@Mapper(componentModel = "spring", uses = {JobMapper.class, EvaluationCriteriaMapper.class})
+public interface CompanyMapper extends EntityMapper<CompanyDTO, CompanyEntity>, VmMapper<CompanyDTO, CompanyIncludeVM> {
 
     @Override
-    @Mapping(source = "file.id",target = "fileId")
+    @Mapping(source = "file.id", target = "fileId")
     CompanyDTO toDto(CompanyEntity entity);
 }

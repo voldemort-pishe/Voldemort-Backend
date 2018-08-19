98d3017fb25ec047aa0fbab0f0419755a24c6365
Pooya <pouyaashna@gmail.com>
2018-08-19 20:24:58 +0430

    add jobIncludeVm and fix mapper

--- src/main/java/io/avand/service/mapper/JobMapper.java
+++ src/main/java/io/avand/service/mapper/JobMapper.java
@@ -2,15 +2,16 @@ package io.avand.service.mapper;
 
 import io.avand.domain.entity.jpa.JobEntity;
 import io.avand.service.dto.JobDTO;
+import io.avand.web.rest.vm.response.JobIncludedVM;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring",uses = {CandidateMapper.class})
-public interface JobMapper extends EntityMapper<JobDTO, JobEntity> {
+@Mapper(componentModel = "spring", uses = {CandidateMapper.class})
+public interface JobMapper extends EntityMapper<JobDTO, JobEntity>, VmMapper<JobDTO, JobIncludedVM> {
 
     @Override
-    @Mapping(source = "company.id",target = "companyId")
-    @Mapping(source = "hiredManager.id",target = "hiredManagerId")
-    @Mapping(source = "hiredExpert.id",target = "hiredExpertId")
+    @Mapping(source = "company.id", target = "companyId")
+    @Mapping(source = "hiredManager.id", target = "hiredManagerId")
+    @Mapping(source = "hiredExpert.id", target = "hiredExpertId")
     JobDTO toDto(JobEntity entity);
 }
---
+++ src/main/java/io/avand/web/rest/vm/response/JobIncludedVM.java
@@ -0,0 +1,82 @@
+package io.avand.web.rest.vm.response;
+
+import java.io.Serializable;
+
+public class JobIncludedVM implements Serializable {
+    private Long id;
+    private String nameFa;
+    private String descriptionFa;
+    private String nameEn;
+    private String descriptionEn;
+    private String location;
+    private String department;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNameFa() {
+        return nameFa;
+    }
+
+    public void setNameFa(String nameFa) {
+        this.nameFa = nameFa;
+    }
+
+    public String getDescriptionFa() {
+        return descriptionFa;
+    }
+
+    public void setDescriptionFa(String descriptionFa) {
+        this.descriptionFa = descriptionFa;
+    }
+
+    public String getNameEn() {
+        return nameEn;
+    }
+
+    public void setNameEn(String nameEn) {
+        this.nameEn = nameEn;
+    }
+
+    public String getDescriptionEn() {
+        return descriptionEn;
+    }
+
+    public void setDescriptionEn(String descriptionEn) {
+        this.descriptionEn = descriptionEn;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "JobIncludedVM{" +
+            "id=" + id +
+            ", nameFa='" + nameFa + '\'' +
+            ", descriptionFa='" + descriptionFa + '\'' +
+            ", nameEn='" + nameEn + '\'' +
+            ", descriptionEn='" + descriptionEn + '\'' +
+            ", location='" + location + '\'' +
+            ", department='" + department + '\'' +
+            '}';
+    }
+}

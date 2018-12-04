bec241e731bf1a06854e5a1b9212cbe0b5427d5c
Pooya <pouyaashna@gmail.com>
2018-12-04 19:26:28 +0330

    add new method to find jobHireTeam by user login

--- src/main/java/io/avand/repository/jpa/JobHireTeamRepository.java
+++ src/main/java/io/avand/repository/jpa/JobHireTeamRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface JobHireTeamRepository extends JpaRepository<JobHireTeamEntity, Long> {
 
     List<JobHireTeamEntity> findByJob_IdAndJob_Company_Id(Long jobId, Long companyId);
+
+    List<JobHireTeamEntity> findAllByUser_LoginAndJob_Id(String login,Long jobId);
 }

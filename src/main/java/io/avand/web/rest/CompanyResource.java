677a05ce6c7be9bed1065d1d8b617b49efba4de9
Pooya <pouyaashna@gmail.com>
2018-12-16 18:18:30 +0330

    remove update company resource

--- src/main/java/io/avand/web/rest/CompanyResource.java
+++ src/main/java/io/avand/web/rest/CompanyResource.java
@@ -82,34 +82,6 @@ public class CompanyResource {
         }
     }
 
-    /**
-     * PUT  /company : Updates an existing companyEntity.
-     *
-     * @param companyDTO the companyEntity to update
-     * @return the ResponseEntity with status 200 (OK) and with body the updated companyEntity,
-     * or with status 400 (Bad Request) if the companyEntity is not valid,
-     * or with status 500 (Internal Server Error) if the companyEntity couldn't be updated
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
-    @PutMapping
-    @Timed
-    @PreAuthorize("isMember(#companyDTO.id,'COMPANY','EDIT_COMPANY')")
-    public ResponseEntity<ResponseVM<CompanyDTO>> updateCompany(@Valid @RequestBody CompanyDTO companyDTO)
-        throws URISyntaxException {
-        log.debug("REST request to update Company : {}", companyDTO);
-        if (companyDTO.getId() == null) {
-            return createCompany(companyDTO);
-        }
-        try {
-            ResponseVM<CompanyDTO> result = companyComponent.save(companyDTO);
-            return ResponseEntity.ok()
-                .headers(HeaderUtil.createEntityUpdateAlert(ENTITY_NAME, companyDTO.getId().toString()))
-                .body(result);
-        } catch (NotFoundException e) {
-            throw new ServerErrorException(e.getMessage());
-        }
-
-    }
 
     /**
      * GET  /company/:id : get the "id" companyEntity.

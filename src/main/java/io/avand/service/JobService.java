1f19dbdd703e990c2df6e3aef0cfc8bf5f6cfdc6
Pooya <pouyaashna@gmail.com>
2018-12-20 18:40:10 +0330

    add new api for publish job

--- src/main/java/io/avand/service/JobService.java
+++ src/main/java/io/avand/service/JobService.java
@@ -1,5 +1,6 @@
 package io.avand.service;
 
+import io.avand.domain.enumeration.JobStatus;
 import io.avand.service.dto.JobDTO;
 import io.avand.web.rest.vm.JobFilterVM;
 import javassist.NotFoundException;
@@ -12,11 +13,15 @@ public interface JobService {
 
     JobDTO save(JobDTO jobDTO) throws NotFoundException;
 
+    JobDTO update(JobDTO jobDTO) throws NotFoundException;
+
+    void publish(Long jobId, JobStatus status) throws NotFoundException;
+
     JobDTO findById(Long id) throws NotFoundException;
 
     List<JobDTO> findAllByCompanySubDomain(String subDomain);
 
-    JobDTO findByJobUniqueIdAndCompanySubDomain(String uniqueId,String subDomain) throws NotFoundException;
+    JobDTO findByJobUniqueIdAndCompanySubDomain(String uniqueId, String subDomain) throws NotFoundException;
 
     Page<JobDTO> findAllByFilter(Pageable pageable, JobFilterVM filterVM) throws NotFoundException;
 

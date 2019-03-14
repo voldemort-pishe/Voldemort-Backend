f2824f2558127ce5d97471abe9787df080797495
ehdi <i@ehdi.in>
2019-03-15 00:12:56 +0330

    Remove domain

--- deployment/scripts/deploy-to-fandogh.sh
+++ deployment/scripts/deploy-to-fandogh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 fandogh service apply \
  -f ./deployment/fandogh-manifests/service-manifest.yaml \
- -p IMAGE_URL \
- -p TAG \
- -p SEC_NAME
+ -p registry.gitlab.com/voldemortapp/voldemort-backend \
+ -p latest \
+ -p gitlab-cred

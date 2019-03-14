743afaf7fdd8b0c89f8e8a6a87ac0973ecee8127
ehdi <i@ehdi.in>
2019-03-15 00:43:25 +0330

    Prepare for Fandogh

--- deployment/scripts/deploy-to-fandogh.sh
+++ deployment/scripts/deploy-to-fandogh.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 fandogh service apply \
- -f ./deployment/fandogh-manifests/service-manifest.yaml \
- -p registry.gitlab.com/voldemortapp/voldemort-backend \
- -p latest \
- -p gitlab-cred
+ -f ./deployment/fandogh-manifests/service-manifest.yaml
